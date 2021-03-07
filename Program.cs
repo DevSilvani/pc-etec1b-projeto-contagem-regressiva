009ec2eb0e2d0d786cf929ece8cb1da307e5afc5
SILVANI CUSTODIO DE OLIVEIRA <silvani.oliveira@etec.sp.gov.br>
2021-03-07 13:19:43 -0300

    C# Pronto

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pc_etec1b_projeto_contagem_regressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("preparar...");
+            Console.ReadKey();
+            Console.WriteLine("Apontar...");
+            Console.ReadKey();
+            Console.WriteLine("Fogo");
+            Console.ReadKey();
+        }
+    }
+}
